1653d971354437ad13c0d9a85f96ee88739b341c
kalpaj12 <kalpaj.agrawalla02@gmail.com>
2018-11-25 18:50:25 +0530

    Initial Commit

---
+++ ConexantFix.bat
@@ -0,0 +1,33 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for admin permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Please Grant Admin Permissions.
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+@echo off
+net stop CxAudioSvc
+net stop CxMonSvc
+net stop CxUtilSvc
+net start CxAudioSvc
+net start CxMonSvc
+net start CxUtilSvc
---
+++ README.md
@@ -0,0 +1,6 @@
+# Conexant-AudioNotPlaying-Fix
+A fix for sound not being played while using Conexant Audio Driver on Windows
+
+1) Clone/Download the repo and run the .bat file.
+
+2) Yup, that's the only step. :)
